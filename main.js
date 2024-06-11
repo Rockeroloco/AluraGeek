a60c540a0f577bcbabe1745ee70a09e8c472ab92
Andres Morales Arce <and0071@gmail.com>
2024-06-11 10:14:34 -0400

    Termini

--- main.js
+++ main.js
@@ -1,4 +1,14 @@
 // main.js
 document.addEventListener('DOMContentLoaded', () => {
     console.log('El DOM ha sido cargado');
+
+    document.querySelector('.mis-productos').addEventListener('click', function(event) {
+        if (event.target.classList.contains('boton-borrar')) {
+            const item = event.target.closest('.productosItem');
+            item.classList.add('fade-out');
+            item.addEventListener('transitionend', function() {
+                item.remove();
+            });
+        }
+    });
 });
